31b38d35d284b5021cde87786e4005e2fbb96542
Raeann <raeann.tsui@gmail.com>
2023-08-30 17:26:42 -0400

    Express Setup Complete

--- backend/app.js
+++ backend/app.js
@@ -12,3 +12,34 @@ const isProduction = environment === 'production';
 
 //Initialize the Express application:
 const app = express();
+
+//Connect the morgan middleware for logging information about requests and responses:
+app.use(morgan('dev'));
+
+//Add the cookie-parser middleware for parsing cookies and the express.json middleware for parsing JSON bodies of requests with Content-Type of "application/json".
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+  
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+  
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
