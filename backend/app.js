488bd8be93e9b90d0a57c4b428bce346aa114642
Raeann <raeann.tsui@gmail.com>
2023-08-30 17:22:49 -0400

    Express Setup Start

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+//Create a variable called isProduction that will be true if the environment is in production or not by checking the environment key in the configuration file (backend/config/index.js):
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//Initialize the Express application:
+const app = express();
