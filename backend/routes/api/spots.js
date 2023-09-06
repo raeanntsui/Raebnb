a8cda744e6d4abf64d05e5f43ab3707d0bdc4a5c
Raeann <raeann.tsui@gmail.com>
2023-09-06 12:53:53 -0400

    3rd endpoint complete with 2nd endpoint commented back in

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -63,50 +63,50 @@ router.get('/', async(req, res) => {
 
 //! Get all Spots owned by the Current User
 //? Need to add authentication portion for this endpoint
-// router.get('/current', async(req, res) => {
-//     // const <plural-of-table-name> = await <singular-form-table-name>.<method-of-search>();
-//     const currentUser = await Spot.findAll({
-//         where: {
-//             ownerId: req.
-//         },
-//         include: [
-//             {
-//                 model: Review
-//             },
-//             {
-//                 model: SpotImage
-//             }
-//         ]
-//     });
+router.get('/current', async(req, res) => {
+    // const <plural-of-table-name> = await <singular-form-table-name>.<method-of-search>();
+    const currentUser = await Spot.findAll({
+        // where: {
+        //     ownerId: req.
+        // },
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage
+            }
+        ]
+    });
 
-//     let currentUserInformation = [];
-//     // console.log("************* Current User Information", currentUserInformation);
-//     currentUserInformation.push(currentUser.toJSON())
+    let currentUserInformation = [];
+    // console.log("************* Current User Information", currentUserInformation);
+    currentUserInformation.push(currentUser.toJSON())
 
-//     const firstUser = currentUserInformation[0];
+    const firstUser = currentUserInformation[0];
     
-//     firstUser.avgRating = 0;
+    firstUser.avgRating = 0;
 
-//     firstUser.Reviews.forEach(oneSpot => {
-//         firstUser.avgRating = firstUser.avgRating + oneSpot.stars;
-//     })
+    firstUser.Reviews.forEach(oneSpot => {
+        firstUser.avgRating = firstUser.avgRating + oneSpot.stars;
+    })
     
-//     firstUser.avgRating = firstUser.avgRating / firstUser.Reviews.length;
-//     if(!firstUser.avgRating) firstUser.avgRating = "No average rating for Spot found.";
-//     delete firstUser.Reviews;
+    firstUser.avgRating = firstUser.avgRating / firstUser.Reviews.length;
+    if(!firstUser.avgRating) firstUser.avgRating = "No average rating for Spot found.";
+    delete firstUser.Reviews;
 
-//     firstUser.SpotImages.forEach(image => {
-//         if (image.preview === true) {
-//             firstUser.previewImage = image.url;
-//         }
-//     })
-//     if (!firstUser.previewImage) firstUser.previewImage = "No URL for preview image for Spot found."
-//     delete firstUser.SpotImages;
+    firstUser.SpotImages.forEach(image => {
+        if (image.preview === true) {
+            firstUser.previewImage = image.url;
+        }
+    })
+    if (!firstUser.previewImage) firstUser.previewImage = "No URL for preview image for Spot found."
+    delete firstUser.SpotImages;
 
 
-//     res.json(currentUserInformation);
-//     // res.json(allSpots);
-// })
+    res.json(currentUserInformation);
+    // res.json(allSpots);
+})
 
 //! Get details of a Spot from an id
 router.get('/:spotId', async(req, res) => {
@@ -133,7 +133,7 @@ router.get('/:spotId', async(req, res) => {
     res.json(allSpots);
 })
 
-router.post('/', async (req, res) => {
-    // const newSpot = await Spot.
-})
+// router.post('/', async (req, res) => {
+//     // const newSpot = await Spot.
+// })
 module.exports = router;
