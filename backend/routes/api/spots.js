1ad1de6cdb97fe1f49e99cb6d3a0bfd7d1b10c05
Raeann <raeann.tsui@gmail.com>
2023-09-06 12:50:24 -0400

    completed 3rd endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { Spot, SpotImage, Review } = require('../../db/models');
+const { Spot, SpotImage, Review, User, Booking, ReviewImage } = require('../../db/models');
+// const { requireAuth } = require('../../utils')
 const router = express.Router();
 
 //! Get All Spots
@@ -62,46 +63,77 @@ router.get('/', async(req, res) => {
 
 //! Get all Spots owned by the Current User
 //? Need to add authentication portion for this endpoint
-router.get('/current', async(req, res) => {
-    // note to self:
-    // const <plural-of-table-name> = await <singular-form-table-name>.<method-of-search>();
-    const currentUser = await Spot.findOne({
-        where: req.query.current,
+// router.get('/current', async(req, res) => {
+//     // const <plural-of-table-name> = await <singular-form-table-name>.<method-of-search>();
+//     const currentUser = await Spot.findAll({
+//         where: {
+//             ownerId: req.
+//         },
+//         include: [
+//             {
+//                 model: Review
+//             },
+//             {
+//                 model: SpotImage
+//             }
+//         ]
+//     });
+
+//     let currentUserInformation = [];
+//     // console.log("************* Current User Information", currentUserInformation);
+//     currentUserInformation.push(currentUser.toJSON())
+
+//     const firstUser = currentUserInformation[0];
+    
+//     firstUser.avgRating = 0;
+
+//     firstUser.Reviews.forEach(oneSpot => {
+//         firstUser.avgRating = firstUser.avgRating + oneSpot.stars;
+//     })
+    
+//     firstUser.avgRating = firstUser.avgRating / firstUser.Reviews.length;
+//     if(!firstUser.avgRating) firstUser.avgRating = "No average rating for Spot found.";
+//     delete firstUser.Reviews;
+
+//     firstUser.SpotImages.forEach(image => {
+//         if (image.preview === true) {
+//             firstUser.previewImage = image.url;
+//         }
+//     })
+//     if (!firstUser.previewImage) firstUser.previewImage = "No URL for preview image for Spot found."
+//     delete firstUser.SpotImages;
+
+
+//     res.json(currentUserInformation);
+//     // res.json(allSpots);
+// })
+
+//! Get details of a Spot from an id
+router.get('/:spotId', async(req, res) => {
+    const allSpots = await Spot.findByPk(req.params.spotId, {
         include: [
             {
-                model: Review
+                model: SpotImage,
+                attributes: ["id", "url", "preview"]
             },
             {
-                model: SpotImage
-            }
+                //! might need to change User => Owner
+                model: User,
+                attributes: ["id", "firstName", "lastName"]
+            },
         ]
     });
 
-    let currentUserInformation = [];
-    console.log("************* Current User Information", currentUserInformation);
-    currentUserInformation.forEach(column => {
-        currentUserInformation.push(column.toJSON())
-    })
-
-    currentUserInformation.forEach(attribute => {
-        attribute.SpotImages.forEach(image => {
-            if (image.preview === true) {
-                attribute.previewImage = image.url;
-            }
+    if (!allSpots) {
+        res.status(404)
+        res.json({
+            message: "Spot couldn't be found."
         })
-        if (!attribute.previewImage) attribute.previewImage = "No URL for preview image for Spot found."
-        delete attribute.SpotImages;
-    })
-
-
-    res.json(currentUser);
-    // res.json(allSpots);
-})
-
-//! Get details of a Spot from an id
-router.get('/:id', async(req, res) => {
-    const allSpots = await Spot.findByPk(req.query.id);
+    }
     res.json(allSpots);
 })
 
+router.post('/', async (req, res) => {
+    // const newSpot = await Spot.
+})
 module.exports = router;
