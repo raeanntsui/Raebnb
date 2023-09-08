1441535b3097e23f4c5734d8d840513ff2d41bc1
Raeann <raeann.tsui@gmail.com>
2023-09-08 00:44:01 -0400

    Edit a Booking: working on booking conflict

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -50,19 +50,86 @@ router.get('/current', requireAuth, async (req, res) => {
 // errors: {
 //     "endDate": "endDate cannot come before startDate"
 //   }
+// router.put('/:bookingId', requireAuth, async (req, res) => {
+//     const booking = await Booking.findByPk(req.params.bookingId)
+
+//     // if a booking currently exists...
+//     if (booking) {
+//         // check if the booking's user id and the current user id are the same
+//         if (booking.userId === req.user.id) {
+//             // if they are the same, continue to edit the booking as intended
+//             const { startDate, endDate } = req.body
+
+//             // converts date from string to object
+//             const editStartDate = new Date(startDate)
+//             const editEndDate = new Date(endDate)
+
+//             // check if the (new end date) editEndDate does not come before the (new start date) editStartDate
+//             if (editStartDate < editEndDate ) {
+//                 // update the booking with the given req.body components
+//                 await booking.update({
+//                     spotId: booking.userId, 
+//                     userId: req.user.id, 
+//                     startDate, 
+//                     endDate
+//                 })
+//                 return res.json(booking)
+//             } else {
+//                 // if the new endDate comes before the new startDate, throw error
+//                 //? Body validation errors
+//                 res.status(400)
+//                 return res.json({
+//                     message: "Bad Request",
+//                     error: "endDate cannot come before startDate"
+//                 })
+//             }
+//         } 
+        
+//         if (booking.userId !== req.user.id) {
+//             // if booking does not have a matching booking user id and current user id
+//             //? Error response: Couldn't find a Booking with the specified id
+//             res.status(404)
+//             return res.json({
+//                 message: "Booking couldn't be found"
+//             })
+//         }
+//         return res.json(booking)
+//     } 
+
+//     //! Error response: Can't edit a booking that's past the end date
+
+// })
+
 router.put('/:bookingId', requireAuth, async (req, res) => {
     const booking = await Booking.findByPk(req.params.bookingId)
+    
+    // if booking does not exist, throw error
+    if (!booking) {
+        res.status(404)
+        return res.json({
+            message: "Booking couldn't be found"
+        })
+    }
+    
+    // if they are the same, continue to edit the booking as intended
+    const { startDate, endDate } = req.body
 
-    // if a booking currently exists...
-    if (booking) {
-        // check if the booking's user id and the current user id are the same
-        if (booking.userId === req.user.id) {
-            // if they are the same, continue to edit the booking as intended
-            const { startDate, endDate } = req.body
+    // converts date from string to object
+    const editStartDate = new Date(startDate)
+    const editEndDate = new Date(endDate)
+    
+    //? Error response: Can't edit a booking that's past the end date
+    const currentBookingDate = new Date()
+    if (currentBookingDate > editEndDate) {
+        res.status(403)
+        return res.json({
+            message: "Past bookings can't be modified"
+        })
+    } 
+    
+    // if a booking currently exists... and if the booking's user id and the current user id are the same
+    if (booking && (booking.userId === req.user.id)) {
 
-            // converts date from string to object
-            const editStartDate = new Date(startDate)
-            const editEndDate = new Date(endDate)
 
             // check if the (new end date) editEndDate does not come before the (new start date) editStartDate
             if (editStartDate < editEndDate ) {
@@ -75,27 +142,14 @@ router.put('/:bookingId', requireAuth, async (req, res) => {
                 })
                 return res.json(booking)
             } else {
-                // if the new endDate comes before the new startDate, throw error
-                //? Body validation errors
+                // if the (new end date) editEndDate comes BEFORE the (new start date) editStartDate, throw error
                 res.status(400)
                 return res.json({
                     message: "Bad Request",
                     error: "endDate cannot come before startDate"
                 })
             }
-
-
-        } else {
-            // if booking does not have a matching booking user id and current user id
-            //? Error response: Couldn't find a Booking with the specified id
-            res.status(404)
-            res.json({
-                message: "Booking couldn't be found"
-            })
-        }
-    } 
-
-    //! Error response: Can't edit a booking that's past the end date
+        } 
 
 })
 
