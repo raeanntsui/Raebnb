17ffc0d32f1ff68bc13de91343bf936018295a21
Raeann <raeann.tsui@gmail.com>
2023-09-10 04:09:19 -0400

    fixed login invalid credentials

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -14,10 +14,10 @@ const validateLogin = [
   check('credential')
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
+    .withMessage('Email or username is required'),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
+    .withMessage('Password is required'),
   handleValidationErrors
 ];
 
@@ -41,7 +41,7 @@ router.post(
       const err = new Error('Login failed');
       err.status = 401;
       err.title = 'Login failed';
-      err.errors = { credential: 'The provided credentials were invalid.' };
+      err.errors = { credential: 'Invalid credentials' };
       return next(err);
     }
 
