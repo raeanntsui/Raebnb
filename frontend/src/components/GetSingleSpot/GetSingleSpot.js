f81f719982bbe0a0bb91f9b5bee1dc57b82269a2
Raeann <raeann.tsui@gmail.com>
2023-09-27 00:24:27 -0700

    added ratings and reviews for 0, 1, or 2+ reviews

--- frontend/src/components/GetSingleSpot/GetSingleSpot.js
+++ frontend/src/components/GetSingleSpot/GetSingleSpot.js
@@ -11,7 +11,6 @@ function ShowSingleSpotDetails() {
 
   const { spotId } = useParams();
 
-  // Dispatch the getSingleSpotThunk with the spotId
   useEffect(() => {
     dispatch(getSingleSpotThunk(spotId));
   }, [dispatch, spotId]);
@@ -25,7 +24,7 @@ function ShowSingleSpotDetails() {
     <>
       <div id="spot-details">
         <h1>Name: {spot.name}</h1>
-        <div>
+        <div id="all-images">
           {spot.SpotImages &&
             spot.SpotImages.map((image) => (
               <img src={image.url} key={image.id} />
@@ -36,7 +35,9 @@ function ShowSingleSpotDetails() {
           {spot.city}, {spot.state}, {spot.country}
         </div>
         <div>
-          <p>Hosted by {spot.Owner && spot.Owner.firstName}</p>
+          <p>
+            Hosted by {spot.Owner && spot.Owner.firstName} {spot.Owner.lastName}
+          </p>
         </div>
         <div>
           <p>Spot description: {spot.description}</p>
@@ -46,6 +47,27 @@ function ShowSingleSpotDetails() {
       <div id="callout-box">
         <div>${spot.price} night</div>
         <button onClick={reserveButtonPopUp}>Reserve</button>
+        <p>
+          {spot.numReviews === 0
+            ? `New`
+            : spot.numReviews === 1
+            ? `1 review`
+            : `${spot.numReviews} reviews`}
+        </p>
+      </div>
+
+      <div>
+        <div>
+          <i class="fa-solid fa-star"></i>
+          {spot.numReviews === 0 ? `No ratings yet!` : `${spot.avgRating}`}
+        </div>
+        <div>
+          {spot.numReviews === 0
+            ? `New`
+            : spot.numReviews === 1
+            ? `1 review`
+            : `${spot.numReviews} reviews`}
+        </div>
       </div>
     </>
   );
